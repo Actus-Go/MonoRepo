02591d06bde289f4c736b3d12e057a79d295fb84
Islam Hany <islam01jul2000@gmail.com>
2024-08-28 04:57:24 +0300

    Setup Swagger for Actus-Go API documentation

---
+++ ActusGo/backend/swagger-docs/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+    openapi: "3.0.0",
+    info: {
+        title: "Actus-Go API documentation",
+    },
+    tags: [
+        {
+            name: "Authentication",
+        },
+        {
+            name: "Email Verification",
+        },
+        {
+            name: "Reset Password",
+        },
+        {
+            name: "Profile",
+        },
+        {
+            name: "Friends And Followers",
+        },
+        {
+            name: "Posts",
+        },
+        {
+            name: "Search And History",
+        },
+    ],
+    paths: {
+        ...require("./auth").paths,
+        ...require("./email-verification").paths,
+        ...require("./reset-password").paths,
+        ...require("./profile").paths,
+        ...require("./friends-and-followers.json").paths,
+        ...require("./posts.json").paths,
+        ...require("./search-and-history.json").paths,
+    },
+};
