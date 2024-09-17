00efe5782fe4c169fe9e7387e2b9cdd9f6519441
DarkCode662432 <ff9371668@gmail.com>
2024-09-18 02:27:26 +0300

    [enhance]

--- ActusGo/frontend/src/components/ExplorePage/ExplorePage.jsx
+++ ActusGo/frontend/src/components/ExplorePage/ExplorePage.jsx
@@ -1,231 +1,73 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { ClapperboardIcon, Image } from "lucide-react";
+import { useState } from "react";
+import MediaPopup from "../Popup";
 
-const sidebarItems = [
-  {
-    src: require("./images/home.png"),
-    text: "Home",
-    className: "w-10 h-10 -ml-2",
-  },
-  {
-    src: require("./images/video-play.png"),
-    text: "Explore",
-    className: "w-8 h-8",
-  },
-  {
-    src: require("./images/location.png"),
-    text: "Map",
-    className: "w-8 h-8 -ml-1",
-  },
-  {
-    src: require("./images/user.png"),
-    text: "Profile",
-    className: "w-10 h-10 -ml-2",
-  },
-  {
-    src: require("./images/diagram.png"),
-    text: "Analytics",
-    className: "w-7 h-7",
-  },
-  {
-    src: require("./images/friends.png"),
-    text: "Friends",
-    className: "w-7 h-7",
-  },
-  {
-    src: require("./images/settings.png"),
-    text: "Settings",
-    className: "w-10 h-10 -ml-2",
-  },
-  {
-    src: require("./images/game-9-svgrepo-com 1.png"),
-    text: "Play Mode",
-    className: "w-10 h-10",
-    type: "checkbox",
-    name: "mode",
-  },
-  {
-    src: require("./images/dark.png"),
-    text: "Dark Mode",
-    className: "w-7 h-7",
-    type: "checkbox",
-    name: "theme",
-  },
-  {
-    src: require("./images/help-svgrepo-com 1.png"),
-    text: "Help Center",
-    className: "w-10 h-10",
-  },
-  {
-    src: require("./images/logout.png"),
-    text: "Logout",
-    className: "w-8 h-8",
-  },
-];
 
-const images = [
-  { src: require("./images/greenSneekers.png"), alt: "Green sneakers" },
-  { src: require("./images/clo.png"), alt: "Colorful clothes" },
-  { src: require("./images/food.png"), alt: "Plate of food" },
-  {
-    src: require("./images/Rectangle 34625174.png"),
-    alt: "Black and white sneakers",
-  },
-  { src: require("./images/pizza.png"), alt: "Pizza" },
-  { src: require("./images/heals.png"), alt: "High heel shoes" },
-  { src: require("./images/f.png"), alt: "Roasted vegetables" },
-  { src: require("./images/books.png"), alt: "Stack of books" },
-  { src: require("./images/vegeta.png"), alt: "Fresh vegetables" },
-];
 
-function Explore() {
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-  const location = useLocation();
+// Main media exploration component
+export default function ExploreMedia({ media }) {
+  const [selectedMedia, setSelectedMedia] = useState(null);
 
-  useEffect(() => {
-    const loadImages = async () => {
-      try {
-        await Promise.all(
-          images.map((image) => {
-            return new Promise((resolve, reject) => {
-              const img = new Image();
-              img.src = image.src;
-              img.onload = resolve;
-              img.onerror = reject;
-            });
-          })
-        );
-        setImagesLoaded(true);
-      } catch (error) {
-        console.error("Error loading images:", error);
-      }
-    };
-
-    loadImages();
-  }, []);
-
-  useEffect(() => {
-    if (imagesLoaded) {
-      console.log("All images loaded successfully");
-    }
-  }, [imagesLoaded]);
-
-  useEffect(() => {
-    console.log("Current path:", location.pathname);
-  }, [location]);
+  const handleMediaSelect = (src, alt, type) => {
+    setSelectedMedia(
+      type === "img" ? (
+        <img
+          src={src}
+          alt={alt}
+          className="max-w-full max-h-full object-contain rounded-lg"
+        />
+      ) : type === "video" ? (
+        <video
+          src={src}
+          controls
+          className="max-w-full max-h-full object-contain w-full rounded-lg"
+        />
+      ) : null
+    );
+  };
 
   return (
-    <div className="flex bg-black min-h-screen w-full text-white justify-between pl-4 md:pl-0 md:items-start my-auto">
-      <aside className="hidden md:block md:w-96 bg-gray-900 transition-all duration-300 ease-in-out">
-        <div className="p-4">
-          <div className="flex justify-between items-center">
-            <img
-              src={require("./images/Logo.png")}
-              alt="logo"
-              className="w-12 md:w-20 h-auto"
-            />
-            <button className="hidden md:block">
-              <img
-                src={require("./images/Vector.png")}
-                alt="close"
-                className="w-4 h-4"
-              />
-            </button>
-          </div>
-          <div className="mt-8 flex items-center flex-col">
-            <img
-              src={require("./images/Avatar.png")}
-              alt="avatar"
-              className="w-28 h-28 rounded-full"
-            />
-            <p className="mt-2 font-semibold">User Name</p>
-          </div>
-          <nav className="mt-8">
-            {sidebarItems.map((item, index) => (
-              <div
-                key={index}
-                className="flex items-center  mt-4 hover:bg-gray-800 py-2 rounded cursor-pointer"
-              >
+    <>
+      <div className="min-h-screen w-11/12 max-w-screen-2xl bg-transparent text-black columns-2 my-10 lg:columns-3 xl:columns-4 gap-5">
+        {media.map(({ src, alt, type }, index) => (
+          <button
+            onClick={() => handleMediaSelect(src, alt, type)}
+            key={index}
+            className="relative break-inside-avoid w-full transition-all mb-5 hover:opacity-80 cursor-pointer"
+          >
+            {type === "img" ? (
+              <>
                 <img
-                  src={item.src}
-                  alt={item.text.toLowerCase()}
-                  className={item.className}
+                  src={src}
+                  alt={alt}
+                  className="w-full rounded-lg shadow-lg"
                 />
-                <p className="ml-4">{item.text}</p>
-                {item.type === "checkbox" && (
-                  <div className="ml-auto mr-4 bg-purple-400 rounded-full flex items-end justify-center ">
-                    <input
-                      className="ml-auto mr-4 bg-purple-400 rounded-full appearance-none w-5 h-5 border-2 border-white checked:bg-white checked:border-purple-400"
-                      type="checkbox"
-                      name={item.name}
-                      value={item.value}
-                      style={{ borderRadius: "50%" }}
-                    />
-                  </div>
-                )}
-              </div>
-            ))}
-          </nav>
-        </div>
-      </aside>
-
-      <div className="flex flex-col items-center justify-between mb-8 md:px-4 mt-4">
-        <div className="flex w-full items-center justify-between mb-8 md:px-4">
-          <div className="hidden md:block relative w-full max-w-[300px]">
-            <input
-              type="text"
-              placeholder="Search on Actus Go ....."
-              className="w-full outline-none focus:ring-2 focus:ring-purple-500 p-2 pl-10 rounded-full bg-black text-black"
-            />
-            <img
-              className="absolute left-2 top-1/2 transform -translate-y-1/2 w-6 h-6"
-              src={require("./images/search.png")}
-              alt="search"
-            />
-          </div>
-          <div className="flex items-center justify-between w-full md:w-auto">
-            <button className="md:hidden">
-              <img
-                src={require("./images/menu.png")}
-                alt="menu"
-                className="w-6 h-6"
-              />
-            </button>
-
-            <img
-              src={require("./images/Logo.png")}
-              alt="logo"
-              className="w-12 md:hidden"
-            />
-            <div className="flex items-center justify-between">
-              <img
-                src={require("./images/people.png")}
-                alt="people"
-                className="w-6 h-6"
-              />
-              <img
-                src={require("./images/bell.png")}
-                alt="bell"
-                className="w-6 h-6"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="columns-2 lg:columns-3 gap-4">
-          {images.map((image, index) => (
-            <div key={index} className="mb-4 break-inside-avoid">
-              <img
-                src={image.src}
-                alt={image.alt}
-                className="w-full rounded-lg shadow-lg hover:opacity-90 transition-opacity"
-              />
-            </div>
-          ))}
-        </div>
+                <span className="p-2 bg-white/80 rounded-md absolute top-5 right-5">
+                  <Image size={24} />
+                </span>
+              </>
+            ) : type === "video" ? (
+              <>
+                <video
+                  src={src}
+                  className="w-full rounded-lg shadow-lg"
+                />
+                <span className="p-2 bg-white/80 rounded-md absolute top-5 right-5">
+                  <ClapperboardIcon size={24} />
+                </span>
+              </>
+            ) : null}
+          </button>
+        ))}
       </div>
-    </div>
+
+      {/* Render Popup if media is selected */}
+      {selectedMedia && (
+        <MediaPopup
+          content={selectedMedia}
+          onClose={() => setSelectedMedia(null)}
+        />
+      )}
+    </>
   );
 }
-
-export default Explore;
---
+++ ActusGo/frontend/src/components/Popup/Popup.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function Popup({ onClose, content }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
+    const handleClose = () => {
+        setIsVisible(false);
+        setTimeout(() => {
+            onClose();
+        }, 100);
+    };
+
+    return (
+        <div className={`fixed inset-0 min-w-screen min-h-screen bg-black/60 transition-opacity flex justify-center items-center ${isVisible ? "opacity-100" : "opacity-30"}`}>
+            {/* Close Button */}
+            <button
+                onClick={handleClose}
+                className="absolute top-10 right-10 w-10 aspect-square bg-white/15 hover:bg-white/90 rounded-full transition-all hover:rotate-360 flex items-center justify-center group"
+            >
+                <span className="absolute w-4/5 h-1 bg-white group-hover:bg-black rounded-full transition-all group-hover:rotate-45" />
+                <span className="absolute w-4/5 h-1 bg-white group-hover:bg-black rounded-full transition-all group-hover:-rotate-45" />
+            </button>
+
+            {/* Content Display */}
+            <div className={`w-4/5 h-4/5 max-w-screen-xl overflow-auto transition-transform flex justify-center items-center ${isVisible ? "scale-100" : "scale-50"}`}>
+                {content}
+            </div>
+        </div>
+    );
+}
