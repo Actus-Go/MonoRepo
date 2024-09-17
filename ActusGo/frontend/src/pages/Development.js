e5eb38924c9c6eb1ee9888e7d16a191a3106d0ed
DarkCode662432 <ff9371668@gmail.com>
2024-09-18 02:31:02 +0300

    [enhance]

--- ActusGo/frontend/src/pages/Development.js
+++ ActusGo/frontend/src/pages/Development.js
@@ -1,15 +1,7 @@
-import { lazy, Suspense } from "react";
-
-const Explore = lazy(() => import("../components/ExploreMedia/ExploreMedia"));
-
 export default function Development() {
-
     return (
         <div className="w-full h-full min-h-screen text-xl font-bold flex justify-center items-center text-center">
-            {/* Explore Section */}
-            <Suspense fallback={<div>Loading...</div>}>
-                <Explore />
-            </Suspense>
+            ++++++++++++++++++++++++++++++++++ TEST ++++++++++++++++++++++++++++++++++
         </div>
     )
 }
