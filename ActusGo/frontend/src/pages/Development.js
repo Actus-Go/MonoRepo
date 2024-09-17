78c2d6a05eb223c5d22f82e7af4fcaaa611389e1
DarkCode662432 <ff9371668@gmail.com>
2024-09-18 02:46:37 +0300

    [enhance]

--- ActusGo/frontend/src/pages/Development.js
+++ ActusGo/frontend/src/pages/Development.js
@@ -1,10 +1,13 @@
-import SuccessCompo from "../components/SuccessCompo/SuccessComo";
+import { useState } from "react";
+import SuccessCompo from "../components/Popup/SuccessPopup";
 
 export default function Development() {
+    const [isSuccess, setSuccess] = useState(false);
+
     return (
         <div className="w-full h-full min-h-screen text-xl font-bold flex justify-center items-center text-center">
-            <button />
-            <SuccessCompo />
+            <button onClick={() => setSuccess(true)}>Show Success</button>
+            {isSuccess && <SuccessCompo onClose={() => {setSuccess(false)}} />}
         </div>
     )
 }
