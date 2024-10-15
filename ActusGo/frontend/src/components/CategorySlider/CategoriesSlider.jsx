8ea6109fca2c9891453f2f149ea1329baaeb918f
DarkCode662432 <ff9371668@gmail.com>
2024-10-15 16:49:52 +0300

    [enhance]

--- ActusGo/frontend/src/components/CategorySlider/CategoriesSlider.jsx
+++ ActusGo/frontend/src/components/CategorySlider/CategoriesSlider.jsx
@@ -7,20 +7,20 @@ import { Link } from 'react-router-dom';
 export default function CategoriesSlider({ categories }) {
   return (
     <div className="p-8 bg-black text-start w-full">
-      <h2 className="text-2xl font-bold text-white mb-6">Categories</h2>
+      <h2 className="text-2xl font-semibold text-white mb-6">Categories</h2>
       <Swiper
         slidesPerView={"auto"}
         spaceBetween={35}
         className="w-full h-full flex justify-start"
       >
         {categories.map(({ url, name, image }, index) => (
-          <SwiperSlide className='w-full max-w-[100px]' key={index}>
+          <SwiperSlide className='w-[100px]' key={index}>
             <Link
               to={url}
               className="flex flex-col justify-center items-center text-white transition-transform transform hover:scale-105"
               aria-label={`Go to ${name} category`}
             >
-              <div className="flex flex-col items-center rounded-full transition">
+              <div className="flex flex-col border border-slate-600 items-center justify-center p-2 rounded-full overflow-hidden aspect-square transition">
                 {image}
               </div>
 
--- ActusGo/frontend/src/pages/Market/index.js
+++ ActusGo/frontend/src/pages/Market/index.js
@@ -1,5 +1,6 @@
 import Slider from '../../components/Slider';
-import Categories from '../../components/'
+import Categories from '../../components/CategorySlider';
+import Offers from '../../components/OffersSlider';
 
 export default function index() {
     let sliderProps = {
@@ -19,8 +20,95 @@ export default function index() {
         ]
     };
 
+    let categoriesProps = {
+        categories: [
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+            {
+                image: <img className='max-w-full max-h-full object-contain' src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' alt=''/>,
+                url: '#',
+                name: 'test'
+            },
+        ]
+    };
+
+    let offersProps = {
+        offers: [
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+            {
+                title: "test",
+                content: [<div className='w-full group transition-all rounded-xl overflow-hidden'><img src='https://iso.500px.com/wp-content/uploads/2015/10/lohi_cover.jpeg' className='min-h-full min-w-full object-cover group-hover:scale-105 transition-all' alt='' /></div>],
+                buy: () => console.log("buy"),
+                details: () => console.log("detials")
+            },
+        ]
+    };
+
     return (
-        <div className='min-h-screen w-[calc(100%-86px)] ml-auto gap-24 flex flex-col p-12 pt-20'>
+        <div className='min-h-screen w-[calc(100%-86px)] ml-auto gap-24 flex flex-col p-12 pt-20 bg-black'>
             <div className='w-full rounded-3xl overflow-hidden'>
                 <Slider {...sliderProps} />
             </div>
@@ -35,7 +123,8 @@ export default function index() {
                 </div>
             </div>
 
-            <Categories />
+            <Categories {...categoriesProps} />
+            <Offers {...offersProps} />
         </div>
     )
 }
