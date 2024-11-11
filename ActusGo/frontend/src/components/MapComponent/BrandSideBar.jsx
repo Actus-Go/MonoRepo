c6c82e71a1d5c6326bcdd3adfecace8171bfbe17
Ahmed Nageh Abdelrazek <ahmednagh2005@gmail.com>
2024-11-11 16:20:42 +0200

    fix conflict

--- ActusGo/frontend/src/components/MapComponent/BrandSideBar.jsx
+++ ActusGo/frontend/src/components/MapComponent/BrandSideBar.jsx
@@ -3,11 +3,7 @@ import MarketView from "./MarketView";
 import { AiOutlineClose } from "react-icons/ai";
 import PropTypes from 'prop-types';
 
-<<<<<<< HEAD
-export default function BrandSideBar({ brand, product, handleExit ,setOpenClosePopup}) {
-=======
 export default function BrandSideBar({price, brand, product, handleExit }) {
->>>>>>> 9b2ce3a2d46ccced7100afc2aa2695bf325f19fd
   return (
     <div
       className={`flex flex-col justify-start items-center z-[999999999999] h-dvh bg-black max-w-[450px] w-full fixed top-0 right-0 overflow-hidden overflow-y-auto transition-all ${product || brand ? "translate-x-0" : "translate-x-full"
@@ -20,11 +16,7 @@ export default function BrandSideBar({price, brand, product, handleExit }) {
         <AiOutlineClose size={20} color="black" />
       </button>
 
-<<<<<<< HEAD
-      {product ? <CouponView {...product} setOpenClosePopup={setOpenClosePopup} /> : <MarketView {...brand} />}
-=======
       {product ? <CouponView {...product} /> : <MarketView price={price} {...brand} />}
->>>>>>> 9b2ce3a2d46ccced7100afc2aa2695bf325f19fd
     </div>
   );
 }
